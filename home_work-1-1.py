5f8d480f31d927fb32ba94119ce76273bd4f2ce6
makeev-rs <ruslanmakeev@gmail.com>
2020-10-09 17:27:40 +1000

    выполнил первое задание первого урока

---
+++ home_work-1-1.py
@@ -0,0 +1,7 @@
+# Задача-1: поработайте с переменными, создайте несколько, выведите на экран.
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+a = int(input("Please enter any number"))
+b = input("what is you name")
+c = float(input("please enter any float"))
+print(a, b, c)
